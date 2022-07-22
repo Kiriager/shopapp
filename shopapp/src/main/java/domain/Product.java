848bc52d422ebbaf87fb6c6c68339ca02e1d924b
honeybadger_ua <kiriager@gmail.com>
2022-07-22 09:37:15 +0300

    create fields in entities

--- shopapp/src/main/java/domain/Product.java
+++ shopapp/src/main/java/domain/Product.java
@@ -1,5 +1,26 @@
 package domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String title;
+  private Double price;
   
+  public Product(String title, Double price) {
+    this.price = price;
+    this.title = title;
+  }
+
+  public Product() {
+  }
+
+  //@ManyToOne
+  //private Location location;
+
 }
--- shopapp/src/main/java/domain/User.java
+++ shopapp/src/main/java/domain/User.java
@@ -1,5 +1,24 @@
 package domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class User {
-  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastrName;
+  private Double amountOfMoney;
+
+  public User(String firstName, String lastrName, Double amountOfMoney) {
+    this.firstName = firstName;
+    this.lastrName = lastrName;
+    this.amountOfMoney = amountOfMoney;
+  }
+
+  public User() {
+  }
 }
