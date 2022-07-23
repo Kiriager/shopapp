02dbbe672a91345d83d0b5fcf244d3e78ef625c8
honeybadger_ua <kiriager@gmail.com>
2022-07-23 13:55:46 +0300

    add product not found exception

--- shopapp/src/main/java/com/challenge/shopapp/exceptions/ProductNotFoundException.java
+++ shopapp/src/main/java/com/challenge/shopapp/exceptions/ProductNotFoundException.java
@@ -1,5 +1,10 @@
 package com.challenge.shopapp.exceptions;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException{
   public ProductNotFoundException(Long id) {
     super("Could not find product with Id = " + id);
